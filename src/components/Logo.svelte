78748a33cb2a2390d2deb4b0874e7f592a5ddee8
jake-pauls <jakepauls99@gmail.com>
2023-06-12 00:37:54 -0600

    added logo and cleaned up some more - most of the base layout is blocked

---
+++ src/components/Logo.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Icon from "svelte-icons-pack/Icon.svelte";
+  import BsLightningChargeFill from "svelte-icons-pack/bs/BsLightningChargeFill";
+
+  /** Color for the heading text in the logo, appends a `tw-text-` prefix */
+  export let textColor = "black";
+  let twTextColor = "tw-text-" + textColor;
+
+  /** Color for the subheading text in the logo, appends a `tw-text-` prefix */
+  export let subheadingTextColor = "grey";
+  let twSubheadingTextColor = "tw-text-" + subheadingTextColor;
+
+  /** todo: find svelte-ish way of settting these props correctly */
+</script>
+
+<!--
+@component
+Main logo component with text aligned in flexboxes.
+-->
+<div class="tw-flex tw-flex-col">
+  <div class="tw-flex tw-flex-wrap tw-items-baseline">
+    <!-- todo: author finer control over sizing -->
+    <p class="tw-font-heading tw-leading-none tw-text-6xl {twTextColor}">
+      Jake Pauls
+    </p>
+    <!-- `Icon` classes are sent to <svg>, color may have to be directly defined here -->
+    <Icon src={BsLightningChargeFill} className="tw-fill-lightning" />
+  </div>
+  <div class="tw-flex">
+    <p class="tw-font-bold tw-text-xs {twSubheadingTextColor}">
+      Graphics Programmer
+    </p>
+  </div>
+</div>
--- src/components/icons/IconButton.svelte
+++ src/components/icons/IconButton.svelte
@@ -1,11 +1,17 @@
 <script>
   import Icon from "svelte-icons-pack/Icon.svelte";
 
+  /** Source of the icon from 'svelte-icons-pack' */
   export let src = undefined;
+
+  /** Size of the underlying icon */
   export let size = "2.55em";
 
   /** Sets an href value, converting the button to a link */
   export let href = "";
+
+  /** Determines whether the link opens in a new tab */
+  export let isExternal = true;
 
   /** Provides class overrides for the icon */
   let className = "";
@@ -24,13 +30,14 @@ An Icon Button that wraps a 'svelte-icons-pack' icon, provides routing to a part
   this={href ? "a" : "button"}
   role={href ? "button" : undefined}
   href={href ? href : undefined}
-  class="icon-button"
+  class="icon-button {className}"
+  target={href && isExternal ? "_blank" : undefined}
   {...$$restProps}
 >
   <Icon
     {src}
     {size}
-    className="tw-fill-white tw-btn-ghost tw-rounded-2xl tw-p-1.5 {className}"
+    className="tw-fill-white tw-btn-ghost tw-rounded-2xl tw-p-1.5"
   />
   <slot />
 </svelte:element>
@@ -42,5 +49,7 @@ An Icon Button that wraps a 'svelte-icons-pack' icon, provides routing to a part
     box-sizing: border-box;
     min-inline-size: 30px;
     min-block-size: 30px;
+    /* https://css-tricks.com/adding-shadows-to-svg-icons-with-css-and-svg-filters/ */
+    filter: drop-shadow(3px 2px 2px rgb(0 0 0 / 0.4));
   }
 </style>
